2e42ed9d58708135b51b5c81562116ea69e27ec0
0davidog <>
2023-10-02 14:36:55 +0000

    Add function to display pizza menu

--- run.py
+++ run.py
@@ -15,8 +15,31 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('pazuzu_pizza')
 
-employee_data = SHEET.worksheet('employees')
+print('Welcome to the Pazuzu Pizza App\n')
 
-data = employee_data.get_all_values()
+def display_pizzas():
+    pizza_menu = SHEET.worksheet('pizza_menu')
+    menu_list = pizza_menu.get_all_values()
+    for pizza in menu_list[1:]:
+        print(*pizza)
+    print('\n')    
+    option_select()
 
-print(data)
+def option_select():
+    print('Please select a function')
+    print('> 1: Display Pizza Menu')
+    print('> 2: Input Sales')
+
+    user_selection = input('Function #: ')
+    if user_selection == "1":
+        print('Display Pizza Menu selected\n')
+        display_pizzas()
+    elif user_selection == "2":
+        print('2 selected, good')
+        option_select()
+    else:
+        print('Please select valid option')
+        option_select() 
+
+option_select()
+
